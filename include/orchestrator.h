72e7ee49b272bab9912cca06f21249fa64671729
Alex Langshur <adlangshur@gmail.com>
2019-08-21 21:20:54 -0500

    Implemented ELO ranking calculation system

--- include/orchestrator.h
+++ include/orchestrator.h
@@ -5,11 +5,17 @@
 
 // define engine types
 typedef int32_t cid;
-typedef int16_t elo;
+typedef float elo;
 
 // define engine manager paramaters
 const size_t INGEST_PACKET_BYTES = 14;
 const size_t INGEST_CID_BYTES = 6;
 const char* const SHUTDOWN_PACKET = "000000 000000";
+
+// define engine injector parameters
+const elo ELO_INITIAL_RATING = 1000.0;
+const float ELO_K_FACTOR = 10.0;
+const float ELO_N_SCALE = 400.0;
+const float ELO_EXP_BASE = 10.0;
 
 #endif
